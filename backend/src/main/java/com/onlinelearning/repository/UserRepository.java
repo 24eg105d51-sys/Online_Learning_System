83be8b621512253b72aa24cf614ab033fc3931ab
24eg105d51-sys <24eg105d51@anurag.edu.in>
2026-09-22 10:23:36 +0530

    Add backend backend/src/main/java/com/onlinelearning/repository/UserRepository.java

---
+++ backend/src/main/java/com/onlinelearning/repository/UserRepository.java
@@ -0,0 +1,3 @@
+package com.onlinelearning.repository;
+import com.onlinelearning.model.User; import org.springframework.data.jpa.repository.JpaRepository; import java.util.Optional;
+public interface UserRepository extends JpaRepository<User,Long>{Optional<User> findByUsername(String username); boolean existsByUsername(String username); boolean existsByEmail(String email);}
